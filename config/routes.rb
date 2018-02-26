2cd7929c146a99c2714dd446cd9af904eb38d858
Christian Meyer <cmeyerwps@gmail.com>
2018-02-25 20:28:07 -0500

    new_root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+
+  get 'foo/baz'
+
   get 'static_pages/home' #displays html from views/home.html.erb
   get 'static_pages/help' #displays html from views/help.html.erb
-  root 'application#hello_page'
+  #root 'application#hello_page'
+  root 'static_pages#home'
 end
