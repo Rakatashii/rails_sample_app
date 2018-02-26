4c17bfffe8dae4d575c4175b617e71eacc942ca4
Christian Meyer <cmeyerwps@gmail.com>
2018-02-25 20:55:04 -0500

    about.html.erb added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,8 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+  
+  #Now that this is added we get yet another error: StaticPagesControllerTest#test_should_get_about: ActionController::UnknownFormat: StaticPagesController#about is missing a template for this request format and variant.
+  def about 
+  end
 end
