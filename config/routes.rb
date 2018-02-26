36dc9c1cd9875f8d72272d259ecba746977868a4
Christian Meyer <cmeyerwps@gmail.com>
2018-02-25 20:50:10 -0500

    various

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
   get 'static_pages/home' #displays html from views/home.html.erb
   get 'static_pages/help' #displays html from views/help.html.erb
-  #root 'application#hello_page'
+  get 'static_pages/about' #with this alone, running 'rails test' gives StaticPagesControllerTest#test_should_get_about: AbstractController::ActionNotFound: The action 'about' 
+  #root 'application#hello_page' could not be found for StaticPagesController - whereas, before this was written, we had:
+  # Error:
+  # StaticPagesControllerTest#test_should_get_about:
+  # NameError: undefined local variable or method 'static_pages_about_url' for ...<..>
   root 'static_pages#home'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
-require 'test_helper'
+require 'test_helper' #where does this come from? What happened to 'minitest/autorun', 'test-unit', + ~rspec?
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+    get static_pages_home_url #how is this case generated
+    assert_response :success #The response :success is an abstract representation of the underlying HTTP status code (in this case, '200 OK')
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
   end
+  
+  # This test should fail
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+  # 3 runs, 2 assertions, 0 failures, 1 errors, 0 skips
 
 end
