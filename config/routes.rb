90ad01baa713b2bb99920e5752634ecea32ddf92
Christian Meyer <cmeyerwps@gmail.com>
2018-02-25 20:08:42 -0500

    help.html.erb changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+  get 'static_pages/home' #displays html from views/home.html.erb
+  get 'static_pages/help' #displays html from views/help.html.erb
   root 'application#hello_page'
 end
