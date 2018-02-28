f4d2ee7e8a0a71ff6555ceeb89f818080bb0ab96
Christian Meyer <cmeyerwps@gmail.com>
2018-02-27 20:07:42 -0500

    integratio_tests_setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
-  get '/help', to: 'static_pages#help'#, 
-  # + , :as 'helf' #This doesn't work!
-  #displays html from views/help.html.erb
+  get '/help', to: 'static_pages#help' #displays html from views/help.html.erb
   get '/about', to: 'static_pages#about' #displays html from views/about.html.erb
   get '/contact', to: 'static_pages#contact' #displays html from views/contact.html.erb
   #compare to old: get 'static_pages/home', etc., which are basically the same but require the longer '/static_pages/' in the URL name.
