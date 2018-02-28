f518b4b2bbd1274900304dffffd61abb73c08533
Christian Meyer <cmeyerwps@gmail.com>
2018-02-27 19:54:23 -0500

    working_routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   test "should get home" do
     #skip
-    get static_pages_home_url #how is this case generated
+    #get static_pages_home_url #how is this case generated
+    get root_path
     assert_select "title", "#{@base_title}"
     assert_response :success 
     #The response :success is an abstract representation of the underlying HTTP status code (in this case, '200 OK')
@@ -14,7 +15,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get help" do
     #skip
-    get static_pages_help_url
+    #get static_pages_help_url
+    get help_path
     assert_select "title", "Help | #{@base_title}"
     assert_response :success
   end
@@ -22,13 +24,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # This test should fail
   test "should get about" do
     #skip
-    get static_pages_about_url
+    #get static_pages_about_url
+    get about_path
     assert_select "title", "About | #{@base_title}"
     assert_response :success
   end
   
   test "should get contact" do
-    get static_pages_contact_url
+    #skip
+    #get static_pages_contact_url
+    get contact_path
     assert_select "title", "Contact | #{@base_title}"
     assert_response :success
   end
