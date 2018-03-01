ca8d8c27e1bc5daa67303ecabffeb495248187a2
Christian Meyer <cmeyerwps@gmail.com>
2018-03-01 18:51:24 -0500

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  # look @ name attribute. The 'presence: true' argument is a one-element options hash.
+  validates :email, presence: true, length: { maximum: 100 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+  # Instance Objects
+  test "should be valid" do
+    assert @user.valid?
+  end
+  # Attribute :name
+  test "name should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  # Attribute :email
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+  test "email should not be too long" do
+    @user.email = "a" * 101
+    assert_not @user.valid?
+  end
 end
