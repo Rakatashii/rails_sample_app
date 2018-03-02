6f92a0e5615932ed8f148c75fa5962cd2745b280
Christian Meyer <cmeyerwps@gmail.com>
2018-03-01 19:40:21 -0500

    new tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, 
+                   length: { maximum: 50 }
   # look @ name attribute. The 'presence: true' argument is a one-element options hash.
-  validates :email, presence: true, length: { maximum: 100 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*(?<!\.)\.[a-z]+\z/i
+  # the 'i' modifier (case-insensitivity) is important here, since, for emails such as email = "foo@baz.com", email.downcase, email.upcase, and email.swapcase are all treated the same (well, really only the domain - i.e., ...@[DOMAIN].com, is treated the same)..
+  # Updated text regexp to account for emails s.a. "foo@bar.baz..com" - previous expression allowed for more than one period. 
+  validates :email, presence: true, 
+                    length: { maximum: 100 }, 
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true, 
+                    case_sensitive: false
+# the :uniqueness validation is case sensitive by default. In order to ensure that duplicate_user fails when duplicate_user.email = @user.email.upcase, we must specify that we want uniqueness to be case insensitive with :case_sensitive set to false.
+                    
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -26,4 +26,26 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 101
     assert_not @user.valid?
   end
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@gmail.com USER@foo.org A_USA-EU@foo.bar.edu]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+      # if @user.valid? == false, display second arg as error message
+    end
+  end
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example foo@bar_baz.com foo@bar+baz.org]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    #Additionally, verifies that the 'i' modifier in the regular expression is doing what it is supposed to
+    duplicate_user.email = @user.email.upcase
+    @user.save # without this, test fails
+    assert_not duplicate_user.valid?
+  end
 end
